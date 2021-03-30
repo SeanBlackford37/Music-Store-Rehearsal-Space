c8fa14d7ac7a339ee02294ce3eca4cf47b528efa
Dani Berman <dberman5@ithaca.edu>
2021-03-29 23:30:57 -0400

    Continued tests

---
+++ src/main/java/edu/ithaca/dragon/bank/Employee.java
@@ -0,0 +1,32 @@
+package edu.ithaca.dragon.bank;
+
+import java.util.ArrayList;
+
+public class Employee {
+    int employeeID;
+    String name;
+    double payAmt;
+
+    public Employee(int employeeID, String name){
+        this.name= name;
+        this.employeeID= employeeID;
+        payAmt= 15.00;
+    }
+
+    public ArrayList<Item> checkStock(String searchItem){
+        
+    }
+
+    public void chargeClient(ArrayList<Item> purchases){
+
+    }
+
+    public void viewSpaceSchedule(){
+
+    }
+
+    public void viewEquipmentSchedule(){
+
+    }
+    
+}
---
+++ src/test/java/edu/ithaca/dragon/bank/EmployeeTest.java
@@ -0,0 +1,22 @@
+package edu.ithaca.dragon.bank;
+
+import org.junit.jupiter.api.Test;
+
+public class EmployeeTest {
+    @Test
+    void checkStockTest(){
+        Employee employee1= new Employee(12345, "Steve");
+        
+
+    }
+
+    void chargeClientTest(){
+
+    }
+
+    void equipmentScheduleTest(){
+
+    }
+
+
+}
