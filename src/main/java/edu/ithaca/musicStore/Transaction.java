11d6b4debc58d5835163eda5f6900fad4a0821de
pseudaimonia <57679024+pseudaimonia@users.noreply.github.com>
2021-03-31 19:25:25 -0400

    adding and removing comments

--- src/main/java/edu/ithaca/musicStore/Transaction.java
+++ src/main/java/edu/ithaca/musicStore/Transaction.java
@@ -4,7 +4,7 @@ import java.util.Calendar;
 import java.util.TimeZone;
 
 public class Transaction {
-    private String dateTime;
+    //private String dateTime;
     private Item itemRented;
     //private Employee seller;
     private Customer buyer;
@@ -14,13 +14,13 @@ public class Transaction {
         if(itemIn==null || buyerIn==null){
             throw new IllegalArgumentException("null argument entered");
         }
-        Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("America/Philadelphia") );
-        dateTime=String.valueOf(cal.get(Calendar.MONTH))+String.valueOf(cal.get(Calendar.DAY_OF_MONTH))
-        +String.valueOf(cal.get(Calendar.YEAR));
+        //Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("America/Philadelphia") );
+        //dateTime=String.valueOf(cal.get(Calendar.MONTH))+String.valueOf(cal.get(Calendar.DAY_OF_MONTH))
+        //+String.valueOf(cal.get(Calendar.YEAR));
         itemRented=itemIn;
         //seller=sellerIn;
         buyer=buyerIn;
-        orderAmount=0;
+        orderAmount=itemIn.getPrice();
     }
 
     public Item getItemRented(){
