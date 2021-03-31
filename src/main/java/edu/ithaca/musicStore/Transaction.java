f8a91e26e351a12c8f421961429475e88f4e1806
pseudaimonia <57679024+pseudaimonia@users.noreply.github.com>
2021-03-31 02:28:32 -0400

    working transaction constructor

--- src/main/java/edu/ithaca/musicStore/Transaction.java
+++ src/main/java/edu/ithaca/musicStore/Transaction.java
@@ -10,7 +10,10 @@ public class Transaction {
     private Customer buyer;
     private double orderAmount;
 
-    public Transaction(Item itemIn, Customer buyerIn){
+    public Transaction(Item itemIn, Customer buyerIn) throws IllegalArgumentException{
+        if(itemIn==null || buyerIn==null){
+            throw new IllegalArgumentException("null argument entered");
+        }
         Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("America/Philadelphia") );
         dateTime=String.valueOf(cal.get(Calendar.MONTH))+String.valueOf(cal.get(Calendar.DAY_OF_MONTH))
         +String.valueOf(cal.get(Calendar.YEAR));
