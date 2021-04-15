cf37581d0c3c0b65d60e628a4dc664f8e9031766
Morgan <morgan.e.ziegler@gmail.com>
2021-04-15 10:05:06 -0400

    RepairItem functions

--- src/main/java/edu/ithaca/musicStore/RepairItem.java
+++ src/main/java/edu/ithaca/musicStore/RepairItem.java
@@ -21,20 +21,43 @@ public class RepairItem {
     }
 
     public void setisBroken(boolean bool){
-        //
+        this.isBroken = bool;
     }
 
     public String getDamageDescription(){
         return damageDescription;
     }
-    
+
     public void displayDamageDescription(){
         System.out.println(damageDescription);
     }
 
+    public static boolean isAmountValid(double balance){
+        String s = "" + balance;
+        String[] result = s.split("\\."); //Splits on the decimal and puts each side into result[1] (left half) and result[2] (right half)
+        if(balance >=0 && result[1].length() <= 2){
+          return true;
+        }
+       return false;
+    }
+
 
     public void setTimeEstimate(double time){
-        //
+       if (isAmountValid(time)){
+           this.timeEstimate = time;
+       }
+       else{
+           throw new IllegalArgumentException("Not a good time");
+       }
+    }
+
+    public void setQuotePrice(double price){
+        if (isAmountValid(price)){
+            this.quotePrice = price;
+        }
+        else{
+            throw new IllegalArgumentException("Not a good money value");
+        }
     }
 
     public String getItemName() {
--- src/test/java/edu/ithaca/musicStore/RepairItemTest.java
+++ src/test/java/edu/ithaca/musicStore/RepairItemTest.java
@@ -29,14 +29,29 @@ public class RepairItemTest {
     void setTimeEstimate(){
         RepairItem item = new RepairItem("Guitar", "Steph", "String broken");
         item.setTimeEstimate(1);
-        assertEquals(1, item.getTimeEstimate());
+        assertEquals(0,Double.compare(1, item.getTimeEstimate()));
         item.setTimeEstimate(1.5);
-        assertEquals(1.5, item.getTimeEstimate());
+        assertEquals(0,Double.compare(1.5, item.getTimeEstimate()));
         item.setTimeEstimate(1.35);
-        assertEquals(1.35, item.getTimeEstimate());
+        assertEquals(0,Double.compare(1.35, item.getTimeEstimate()));
 
         assertThrows(IllegalArgumentException.class, ()-> item.setTimeEstimate(12.403));
         assertThrows(IllegalArgumentException.class, ()-> item.setTimeEstimate(-12.4));
         assertThrows(IllegalArgumentException.class, ()-> item.setTimeEstimate(-12.402));
+    }
+
+    @Test
+    void setQuotePrice(){
+        RepairItem item = new RepairItem("Guitar", "Steph", "String broken");
+        item.setQuotePrice(1);
+        assertEquals(0,Double.compare(1, item.getQuotePrice()));
+        item.setQuotePrice(1.5);
+        assertEquals(0,Double.compare(1.5, item.getQuotePrice()));
+        item.setQuotePrice(1.35);
+        assertEquals(0,Double.compare(1.35, item.getQuotePrice()));
+
+        assertThrows(IllegalArgumentException.class, ()-> item.setQuotePrice(12.403));
+        assertThrows(IllegalArgumentException.class, ()-> item.setQuotePrice(-12.4));
+        assertThrows(IllegalArgumentException.class, ()-> item.setQuotePrice(-12.402));
     }
 }
