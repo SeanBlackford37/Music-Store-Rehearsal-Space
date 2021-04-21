5e1bd8d3722bb551fcc76b586f1fcc61d65a2322
pseudaimonia <57679024+pseudaimonia@users.noreply.github.com>
2021-04-20 21:35:59 -0400

    equipment tests

---
+++ src/main/java/edu/ithaca/musicStore/Equipment.java
@@ -0,0 +1,17 @@
+package edu.ithaca.musicStore;
+
+public class Equipment {
+    private String equipmentName;
+    private String repairTechName;
+    private double price;
+
+    public Equipment(String equipmentName, double price) throws IllegalArgumentException{
+
+    }
+    public Equipment(String equipmentName, double price, String repairTechName) throws IllegalArgumentException{}
+
+    
+    
+
+    
+}
---
+++ src/test/java/edu/ithaca/musicStore/EquipmentTest.java
@@ -0,0 +1,54 @@
+package edu.ithaca.musicStore;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class EquipmentTest {
+    @Test
+    void constructorTest(){
+        Equipment e =  new Equipment("guitar strings",5.25);
+        assertNotEquals(null,e);
+        assertEquals("guitar strings", e.getEquipmentName());
+        assertEquals(5.0, e.getPrice());
+        assertEquals("n/a", e.getRepairTechName());
+
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("guitar strings",-5));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("guitar strings",-5.34));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("guitar strings",-5.345));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("guitar strings",5.345));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("",-5));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("",-5.34));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("",-5.345));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("",5.345));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("",5));
+
+
+        Equipment e2 =  new Equipment("trumpet valve",15.0, "Steph");
+        assertNotEquals(null,e2);
+        assertEquals("trumpet valve", e2.getEquipmentName());
+        assertEquals(15.0, e2.getPrice());
+        assertEquals("Steph", e2.getRepairTechName());
+
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("guitar strings",-5,"Steph"));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("guitar strings",-5.34,"Steph"));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("guitar strings",-5.345,"Steph"));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("guitar strings",5.345,"Steph"));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("",-5,"Steph"));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("",-5.34,"Steph"));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("",-5.345,"Steph"));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("",5.345,"Steph"));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("",5,"Steph"));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("guitar strings",-5,""));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("guitar strings",-5.34,""));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("guitar strings",-5.345,""));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("guitar strings",5.345,""));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("",-5,""));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("",-5.34,""));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("",-5.345,""));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("",5.345,""));
+        assertThrows(IllegalArgumentException.class, ()->new Equipment("",5,""));
+
+    }
+
+}
